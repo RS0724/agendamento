d46c2c9d4ac68b516c6451d51bf70ad0cf3cc169
RS0724 <rosesilva0724@gmail.com>
2023-11-09 21:50:25 -0300

    feat: add style partial page login

--- src/components/BoxWhiteLogin/index.tsx
+++ src/components/BoxWhiteLogin/index.tsx
@@ -1,7 +1,14 @@
 import "./style.css";
 
 const BoxWhiteLogin = () => {
-  return <div id="boxWhiteLogin"></div>;
+  return <div id="boxWhiteLogin">
+    <h1 id="title-app">Agendamento de Ambiente Educaional</h1>
+    <div id="boxWhiteLogin-gradient">
+      <img id="art" src="public\art.png" alt="mulher com computador olhando calendario" />
+    </div>
+    <img id="logo-senai" src="public\logo-senai.png" alt="mulher com computador olhando calendario" />
+      <img id="art-mobile" src="public\art-mobile.png" alt="mulher com computador olhando calendario" />
+  </div>;
 };
 
 export default BoxWhiteLogin;
