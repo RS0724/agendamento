dac78e612a1f0a5b7d77ffc728e9dcb30a0a5af2
RS0724 <rosesilva0724@gmail.com>
2023-11-13 19:18:54 -0300

    feat: center title-login

--- src/components/BoxWhiteLogin/index.tsx
+++ src/components/BoxWhiteLogin/index.tsx
@@ -1,14 +1,25 @@
 import "./style.css";
 
 const BoxWhiteLogin = () => {
-  return <div id="boxWhiteLogin">
-    <h1 id="title-app">Agendamento de Ambiente Educaional</h1>
-    <div id="boxWhiteLogin-gradient">
-      <img id="art" src="public\art.png" alt="mulher com computador olhando calendario" />
+  return (
+    <div id="boxWhiteLogin">
+      <h1 id="title-app">Agendamento de Ambiente Educaional</h1>
+      <h2 id="title-login">Login</h2>
+      <div id="boxWhiteLogin-gradient">
+        <img
+          id="art"
+          src="public\art.png"
+          alt="mulher com computador olhando calendario"
+        />
+      </div>
+      <img id="logo-senai" src="public\logo-senai.png" alt="logomarca senai" />
+      <img
+        id="art-mobile"
+        src="public\art-mobile.png"
+        alt="mulher com computador olhando calendario"
+      />
     </div>
-    <img id="logo-senai" src="public\logo-senai.png" alt="mulher com computador olhando calendario" />
-      <img id="art-mobile" src="public\art-mobile.png" alt="mulher com computador olhando calendario" />
-  </div>;
+  );
 };
 
 export default BoxWhiteLogin;
